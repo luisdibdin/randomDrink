e8efcec5a44538a72d30bcbb0400db3e5b3b2fa5
Luis Dibdin <luisdibdin@luiss-mbp.home>
2022-03-25 00:55:37 +0000

    tried to improve readability of reduce_ratio for Harvey maybe I didn't and he will shout at me

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import List
 import numpy as np
 
 spirits = ["Vodka", "Rum", "White Rum", "Spiced Rum", "Gin", "Absinthe", "Whiskey", "Brandy", "Tequila", "Sambuka"]
@@ -6,7 +5,7 @@ mixers = ["Coke", "Fanta", "Orange Juice", "Chocolate Milk", "Oatly", "Water", "
 
 players = ["Ben", "Sam", "Luis", "Quandale"]
 
-def reduced_ratio(int1: int, int2: int) -> int:
+def reduce_ratio(int1: int, int2: int) -> int:
     gcd = np.gcd.reduce([int1, int2])
 
     return int(int1/gcd), int(int2/gcd)
@@ -18,6 +17,6 @@ for i in range(len(players)):
     player = players[i]
     spirit = spirits[i]
     mixer = mixers[i]
-    ratio1, ratio2 = reduced_ratio(np.random.randint(3, size=1)[0] + 1, np.random.randint(5, size=1)[0] + 1)
+    ratio1, ratio2 = reduce_ratio(np.random.randint(3, size=1)[0] + 1, np.random.randint(5, size=1)[0] + 1)
 
     print(f"{player} will be drinking {spirit} with {mixer} at a ratio of {ratio1}:{ratio2}")
